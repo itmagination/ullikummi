57f70eb6ab28fb406bde576554a3577f41f4375c
Wojciech Stelmaszewski <stelmaszewskiw@gmail.com>
2017-02-26 11:37:22 +0100

    GraphConst moved to Ullikummi.Data project.

--- Ullikummi.CodeGeneration/RoslynCodeGenerator.cs
+++ Ullikummi.CodeGeneration/RoslynCodeGenerator.cs
@@ -10,28 +10,6 @@ using Ullikummi.CodeGeneration.Objective.Roslyn.Code;
 
 namespace Ullikummi.CodeGeneration
 {
-    public static class GraphConst
-    {
-        public class Metadata
-        {
-            public const string Name = "name";
-            public const string Namespace = "namespace";
-            public const string Using = "using";
-            public const string Accessibility = "accessibility";
-
-            public const string UsingNamespaceSeparator = "&";
-        }
-
-        public class ConnectionProperties
-        {
-            public const string Name = "name";
-            public const string Parameters = "params";
-
-            public const string ValuesSeparator = ";";
-            public const string ComplexValueSeparator = "#";
-        }
-    }
-
     public static class GraphExtensions
     {
         public const string DefaultName = "Ullikummi";
---
+++ Ullikummi.Data/GraphConst.cs
@@ -0,0 +1,24 @@
+﻿namespace Ullikummi.Data
+{
+    public static class GraphConst
+    {
+        public class Metadata
+        {
+            public const string Name = "name";
+            public const string Namespace = "namespace";
+            public const string Using = "using";
+            public const string Accessibility = "accessibility";
+
+            public const string UsingNamespaceSeparator = "&";
+        }
+
+        public class ConnectionProperties
+        {
+            public const string Name = "name";
+            public const string Parameters = "params";
+
+            public const string ValuesSeparator = ";";
+            public const string ComplexValueSeparator = "#";
+        }
+    }
+}
